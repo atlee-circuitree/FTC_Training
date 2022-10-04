d895d5736dd160da3f63dbfa5214077dc8be7ccb
Atlee Desktop 1 <ahsrobotics@hcps.us>
2022-10-04 16:34:35 -0400

    Update While_LoopsElapsed_Time.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/While_LoopsElapsed_Time.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/While_LoopsElapsed_Time.java
@@ -49,7 +49,7 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Jameson opmode", group="Linear Opmode")
+@TeleOp(name="Jameson opmode whileloop", group="Linear Opmode")
 public class While_LoopsElapsed_Time extends LinearOpMode {
 
     // Declare OpMode members.
@@ -85,38 +85,38 @@ public class While_LoopsElapsed_Time extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            if (gamepad1.x && servoActive == false)
-            {
+            if (gamepad1.x && servoActive == false) {
 
 
-            // Setup a variable for each drive wheel to save power level for telemetry
-            double leftPower;
-            double rightPower;
+                // Setup a variable for each drive wheel to save power level for telemetry
+                double leftPower;
+                double rightPower;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+                // Choose to drive using either Tank Mode, or POV Mode
+                // Comment out the method that's not used.  The default below is POV.
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+                // POV Mode uses left stick to go forward, and right stick to turn.
+                // - This uses basic math to combine motions and is easier to drive straight.
+                double drive = -gamepad1.left_stick_y;
+                double turn = gamepad1.right_stick_x;
+                leftPower = Range.clip(drive + turn, -1.0, 1.0);
+                rightPower = Range.clip(drive - turn, -1.0, 1.0);
 
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
+                // Tank Mode uses one stick to control each wheel.
+                // - This requires no math, but it is hard to drive forward slowly and keep straight.
+                // leftPower  = -gamepad1.left_stick_y ;
+                // rightPower = -gamepad1.right_stick_y ;
 
-            // Send calculated power to wheels
-            leftDrive1.setPower(leftPower);
-            leftDrive2.setPower(rightPower);
-            rightDrive1.setPower(leftPower);
-            rightDrive2.setPower(rightPower);
-            // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
-            telemetry.update();
+                // Send calculated power to wheels
+                leftDrive1.setPower(leftPower);
+                leftDrive2.setPower(rightPower);
+                rightDrive1.setPower(leftPower);
+                rightDrive2.setPower(rightPower);
+                // Show the elapsed game time and wheel power.
+                telemetry.addData("Status", "Run Time: " + runtime.toString());
+                telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+                telemetry.update();
+            }
         }
     }
 }
